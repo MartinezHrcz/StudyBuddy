a1f4a5e9e08cf5caaafb5e86f7ba51a358a494be
Herczeg Martinez <herczegmartinez@gmail.com>
2025-10-28 17:24:44 +0100

    style: register page updated

--- project/frontend/src/pages/Register.jsx
+++ project/frontend/src/pages/Register.jsx
@@ -42,7 +42,6 @@ export default function Register() {
                       className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
                   />
 
-                  {/* Password */}
                   <input
                       type="password"
                       placeholder="Password"
@@ -51,7 +50,6 @@ export default function Register() {
                       className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
                   />
 
-                  {/* Register Button */}
                   <button
                       onClick={handleRegister}
                       className="w-full bg-green-600 text-white py-2 rounded-lg hover:bg-green-700 transition"
